49b48d1c0b84c2a6e4ed2c0c18a8dd57846ddd4a
Vikki Chan <vikkiwchan@gmail.com>
2021-03-02 14:30:09 -0600

    package and webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+const path = require('path');
 module.exports = {
   module: {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'main.js',
+    },
     rules: [
       {
         test: /\.js$/,
